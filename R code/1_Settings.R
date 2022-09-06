bc3cb684ab76b92675adccd5ac6a3276555f63c2
csoyn <csy3981@gamil.com>
2022-09-06 17:35:29 +0900

    라이브러리와 DB 연결을 위한 code - 우선으로 실행되어져야함

---
+++ R code/1_Settings.R
@@ -0,0 +1,47 @@
+
+# --------------- Settings.R --------------------
+
+#기본적으로 분석에 사용할 패키키들을 불러오고,
+# DB 연결을 합니다.
+
+# 1. LIBRARY
+library(devtools)
+library(SqlREnder)
+library(DatabaseConnector)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(forecast)
+library(ggrepel)
+library(scales)
+
+# 1-1. LIBRARY 없을 시 설치
+# install.packages("devtools")
+# install.packages("SqlREnder")
+# install.packages("DatabaseConnector")
+# install.packages("dplyr")
+# install.packages("lubridate")
+# install.packages("ggplot2")
+# install.packages("forecast")
+# install.packages("ggrepel")
+# install.packages("scales")
+
+
+
+# 2. 심사평가원 DB 연결
+# TODO : 데이터베이스 연결 파라미터 변수 5종 입력 
+dbms <- ""
+user <- ""
+password <- ""
+server <- ""
+pathToDriver <- ""
+
+connectionDetails <- createConnectionDetails(dbms=dbms,
+                                             user=user,
+                                             password=password,
+                                             server=server,
+                                             pathToDriver = pathToDriver)
+conn <- connect(connectionDetails)
+
+# 3. etc setting
+options(scipen=100)
