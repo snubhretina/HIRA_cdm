f677942b8e542dfa1279f4f8c4ada60366368e55
LDY <lee40531768@gmail.com>
2022-09-14 13:41:29 +0900

    sql query 추가

--- R code/3_분석_COX/8_COX_timevar.R
+++ R code/3_분석_COX/8_COX_timevar.R
@@ -7,7 +7,7 @@ library(dplyr)
 library(lubridate)
 library(survival)
 
-#parameter
+# parameter
 cdm_schema <- ""
 cdm_voca_schema <- ""
 researcher_schema <- ""
@@ -55,6 +55,63 @@ covidAllSqlquery <- function(researcher){
   )
 }
 
+covidRownumSqlquery <- function(researcher){
+  sql_1 <- "create table "
+  sql_2 <- "covid_data_rownum as select a.*, row_number() over (partition by a.person_id order by a.covid_date) as p_row_num from "
+  sql_3 <- "covid_data_all a;"
+  researcher <- paste0(researcher, '.')
+  return(
+    paste0(sql_1, researcher, sql_2, researcher, sql_3)
+  )
+}
+
+covidDataSqlquery <- function(researcher){
+  sql_1 <- "create table "
+  sql_2 <- "covid_data_todo as select * from "
+  sql_3 <- "covid_data_rownum where p_row_num in (1);"
+  researcher <- paste0(researcher, '.')
+  return(
+    paste0(sql_1, researcher, sql_2, researcher, sql_3)
+  )
+}
+
+# RVO data
+rvoCondSqlquery <- function(researcher, cdm, cdm_voca, concept_ids){
+  sql_1 <- "create table "
+  sql_2 <- "rvo_data as select person_id, condition_concept_id, condition_start_date from "
+  sql_3 <- "condition_occurrence where condition_concept_id in (select descendant_concept_id from "
+  sql_4 <- "concept_ancestor where ancestor_concept_id in ("
+  researcher <- paste0(researcher, '.')
+  cdm <- paste0(cdm, '.')
+  cdm_voca <- paste0(cdm_voca, '.')
+  concept_ids <- concept_ids
+  return(
+    paste0(sql_1, researcher, sql_2, cdm, sql_3, cdm_voca, sql_4, concept_ids, '));')
+  )
+}
+
+rvoRownumSqlquery <- function(researcher){
+  sql_1 <- "create table "
+  sql_2 <- "rvo_data_rownum as select a.*, row_number() over (partition by a.person_id order by a.condition_start_date) as p_row_num from "
+  sql_3 <- "rvo_data a;"
+  researcher <- paste0(researcher, '.')
+  return(
+    paste0(sql_1, researcher, sql_2, researcher, sql_3)
+  )
+}
+
+rvoDataSqlquery <- function(researcher){
+  sql_1 <- "create table "
+  sql_2 <- "rvo_data_todo as select * from "
+  sql_3 <- "rvo_data_rownum where p_row_num in (1);"
+  researcher <- paste0(researcher, '.')
+  return(
+    paste0(sql_1, researcher, sql_2, researcher, sql_3)
+  )
+}
+
+# Washout
+
 {
   sqlquery_covid_obs <- covidObsSqlquery(researcher_schema, cdm_schema, covid_observation_id)
   sqlquery_covid_obs
@@ -70,9 +127,37 @@ covidAllSqlquery <- function(researcher){
   sqlquery_covid_all
   executeSql(conn, sqlquery_covid_all) ## 에러가 난다면 아래 주석 풀어서 실행 후 다시 실행
   # executeSql(conn, paste0("drop table ", researcher_schema, ".covid_data_all;"))
+  
+  sqlquery_covid_rownum <- covidRownumSqlquery(researcher_schema)
+  sqlquery_covid_rownum
+  executeSql(conn, sqlquery_covid_rownum) ## 에러가 난다면 아래 주석 풀어서 실행 후 다시 실행
+  # executeSql(conn, paste0("drop table ", researcher_schema, ".covid_data_rownum;"))
+  
+  sqlquery_covid_todo <- covidDataSqlquery(researcher_schema)
+  sqlquery_covid_todo
+  executeSql(conn, sqlquery_covid_todo) ## 에러가 난다면 아래 주석 풀어서 실행 후 다시 실행
+  # executeSql(conn, paste0("drop table ", researcher_schema, ".covid_data_todo;"))
+  
+  ############################################################################################
 
-  paste0("create table ", researcher_schema, ".covid_data_rownum as select a.*, row_number() over (partition by a.person_id order by a.covid_date) as p_row_num from ", researcher_schema, ".covid_data_all a;")
+  sqlquery_rvo_cond <- rvoCondSqlquery(researcher_schema, cdm_schema, cdm_voca_schema, rvo_concept_id)
+  sqlquery_rvo_cond
+  executeSql(conn, sqlquery_rvo_cond) ## 에러가 난다면 아래 주석 풀어서 실행 후 다시 실행
+  # executeSql(conn, paste0("drop table ", researcher_schema, ".rvo_data;"))
+  
+  sqlquery_rvo_rownum <- rvoRownumSqlquery(researcher_schema)
+  sqlquery_rvo_rownum
+  executeSql(conn, sqlquery_rvo_rownum) ## 에러가 난다면 아래 주석 풀어서 실행 후 다시 실행
+  # executeSql(conn, paste0("drop table ", researcher_schema, ".rvo_data_rownum;"))
+  
+  sqlquery_rvo_todo <- rvoDataSqlquery(researcher_schema)
+  sqlquery_rvo_todo
+  executeSql(conn, sqlquery_rvo_todo) ## 에러가 난다면 아래 주석 풀어서 실행 후 다시 실행
+  # executeSql(conn, paste0("drop table ", researcher_schema, ".rvo_data_todo;"))
 }
+
+#paste0("create table ", researcher_schema, ".covid_data_rownum as select a.*, row_number() over (partition by a.person_id order by a.covid_date) as p_row_num from ", researcher_schema, ".covid_data_all a;")
+
 
 
 # 데이터 행. 사람 수 확인
