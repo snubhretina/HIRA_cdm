962a7e3ecd500bc14d9231d74e7ad6800e185653
csoyn <76737270+csoyn@users.noreply.github.com>
2022-09-14 15:28:28 +0900

    Update 3_RD_Vitrectomy_Buckling.R
    typo & 저장경로 수정

--- R code/1_분석_ITS/3_RD_Vitrectomy_Buckling.R
+++ R code/1_분석_ITS/3_RD_Vitrectomy_Buckling.R
@@ -1,17 +1,25 @@
 # condition RD + Procedure Vitrectomy, buckling
-source("R code/1_Settings.R")
-source("R code/2_Functions.R")
+# TODO : 현재 작업 위치 (1_Settings.R를 포함하여 R 파일들이 존재하는 폴더명으로 작성해주세요)
+path <- ""
+setwd(path)
+source("1_Settings.R")
+source("2_Functions.R")
 
-# data 만들기
-# TODO : START_DATE
+
+
+# ---------------------------- Cohort -----------------------------
+# data 만들기 
 rd <- conditionCohort("4055484", "2018-01-01")
 rd_data <- makeData_C(rd)
 rd_used_id <- makeIdTable_C(rd, rd_data)
 
-# TODO : START_DATE
 vi_buck <- procedureCohort("4294683, 4230961", "2018-01-01")
 vi_buck_data <- makeData_P(vi_buck)
 vd_used_id <- makeIdTable_P(vi_buck, vi_buck_data)
+
+# 사용된 Concept ID 확인을 위해 저장합니다.
+used_id3 <- rbind(rd_used_id, vd_used_id)
+write.csv(used_id3, file="./result_3/3_used_id")
 
 check_rd <- checkData(rd_data)
 check_vi <- checkData(vi_buck_data)
@@ -21,6 +29,8 @@ PERSON <- c(check_rd["person"], check_vi["person"])
 ROW <- c(check_rd["row"], check_vi["row"])
 checked_table <- data.frame(PERSON, ROW)
 
+
+# ---------------------------- Analysis -----------------------------
 
 # select first
 rd_data_s <- selectFirst(rd_data, "CONDITION_START_DATE")
@@ -50,20 +60,20 @@ join_data["ALL_COUNT"] = join_data["RD"] + join_data["Vitrectomy or Buckling"]
 
 # plot
 min_date_rd = min(join_data$UNIT_DATE)
-max_date_rd = min(join_data$UNIT_DATE)
+max_date_rd = max(join_data$UNIT_DATE)
 
 # 반출할 csv에 컬럼추가
 checked_table[,"MIN_DATE"] <- min_date_rd
 checked_table[,"MAX_DATE"] <- max_date_rd
 # csv 저장
-write.csv(checked_table, file="3_dataAndDate")
+write.csv(checked_table, file="./result_3/3_dataAndDate")
 
 
 # plot x축 범위 만들어 놓기
 date_breaks <- seq(as.Date(min_date_rd), as.Date(max_date_rd), by="6 month")
 
 getwd()
-pdf("./RD_Vitrectomy_Buckling_plots/pdf")
+pdf("./result_3/3_RD_Vitrectomy_Buckling_plots.pdf", width = 25)
 # all
 plot <- ggplot2::ggplot(data = join_data, aes(x=UNIT_DATE, y=ALL_COUNT)) + geom_line(size=1)
 plot + labs(title=" RD Vitrectomy or Buckling Weekly Count", x="date", y="weekly count") + scale_x_date( breaks = date_breaks, labels = date_format("%y-%m-%d"))
