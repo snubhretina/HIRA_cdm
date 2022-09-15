0c3a32f07c485a11428ea4414e95ee9750fe31b0
LDY <lee40531768@gmail.com>
2022-09-15 17:44:33 +0900

    code 수정

--- R code/2_분석_eAMD/7_eAMD.R
+++ R code/2_분석_eAMD/7_eAMD.R
@@ -2,12 +2,12 @@
 source("R code/1_Settings.R")
 source("R code/2_Functions.R")
 
-cdm_schema <- ""     # 1. cdm_schema
-cdm_voca_schema <- ""  # 2. cdm_voca_schema
+cdm_schema <- "cdm_2020_view"     # 1. cdm_schema
+cdm_voca_schema <- "cdm_voca"  # 2. cdm_voca_schema
 
 # data 만들기
 # TODO : START_DATE
-eamd <- conditionCohort("4146103", "2018-07-01")
+eamd <- conditionCohort("4146103")
 eamd_data <- makeData_C(eamd)
 eamd_used_id <- makeIdTable_C(eamd, eamd_data)
 
@@ -20,24 +20,146 @@ eylea_data <- makeData_D(eylea)
 
 lucentis_ID_data <- makeIdTable_D(lucentis, lucentis_data)
 eylea_ID_data <- makeIdTable_D(eylea, eylea_data)
+lucentis_ID_data["DRUG_CLASS"] = "Lucentis"
+eylea_ID_data["DRUG_CLASS"] = "Eylea"
+
+used_id7 <- rbind(lucentis_ID_data, eylea_ID_data)
+write.csv(used_id7, file="./result_eamd/used_id")
+
+used_lucentis_id <- lucentis_ID_data$DES_ID
+used_eylea_id <- eylea_ID_data$DES_ID
+used_drug_id <- c(used_lucentis_id, used_eylea_id)
 
 check_lucentis <- checkData(lucentis_data)
 check_eylea <- checkData(eylea_data)
 
-# Washout
-washoutQuery <- function(ancestor_ids, washout_end_date) {
-  sql_1 <- "select person_id, condition_concept_id, condition_start_date from " 
-  sql_2 <- "condition_occurrence where condition_concept_id in (select descendant_concept_id from "
-  sql_3 <- "concept_ancestor where ancestor_concept_id in ("
-  cdm_schema <- paste0(cdm_schema, '.')  
-  cdm_voca_schema <- paste0(cdm_voca_schema, '.')
-  ancestor_ids <- ancestor_ids
-  sql_4 <- ") and condition_start_date <="
-  washout_end_date <- paste0("TO_DATE('", washout_end_date, "', 'YYYY-MM-DD')") 
-  return (
-      #data = querySql(conn, paste0(sql_1, cdm_schema, sql_2, cdm_voca_schema, sql_3, ancestor_ids, '));'))
-      paste0(sql_1, cdm_schema, sql_2, cdm_voca_schema, sql_3, ancestor_ids, sql_4, washout_end_date, ');')
-  )
-}
-washout_id <- querySql(conn, washoutQuery("4146103", "2018-06-30"))
+drug_data <- rbind(lucentis_data, eylea_data)
+check_drug <- checkData(drug_data)
 
+# Washout
+washout_id <- querySql(conn, washoutIdQuery("4146103", "2018-06-30"))
+eamd_data_todo <- eamd_data %>% filter(!PERSON_ID %in% washout_id$PERSON_ID)
+
+# Data todo
+drug_data_todo <- drug_data %>% filter(PERSON_ID %in% eamd_data_todo$PERSON_ID)
+
+# 전체 주사횟수
+#연도별
+drug_data_todo$year <- format(as.Date(drug_data_todo$DRUG_EXPOSURE_START_DATE), "%Y")
+
+all <- drug_data_todo %>%
+  group_by(year) %>%
+  summarise(count_n = n_distinct(PERSON_ID), count_inj = n())
+
+luce <- drug_data_todo %>%
+  filter(DRUG_CONCEPT_ID %in% used_lucentis_id) %>%
+  group_by(year) %>%
+  summarise(count_n = n_distinct(PERSON_ID), count_inj = n())
+
+eyle <- drug_data_todo %>%
+  filter(DRUG_CONCEPT_ID %in% used_eylea_id) %>%
+  group_by(year) %>%
+  summarise(count_n = n_distinct(PERSON_ID), count_inj = n())
+
+mergeAll <- merge(merge(
+  all,
+  rani, by = 'year', all = TRUE),
+  afli, by = 'year', all = TRUE)
+
+colnames(mergeAll) <- c("YEAR", 
+                        "ALLDRUG_N", "ALLDRUG_INJ",
+                        "RANI_N", "RANI_INJ", 
+                        "AFLI_N", "AFLI_INJ")
+
+write.csv(mergeAll, file = "./result_eamd/all_drug_count_by_year.csv")
+
+#월별
+drug_data_todo["UNIT_DATE"] = floor_date(drug_data_todo$DRUG_EXPOSURE_START_DATE, unit="month")
+
+all_m <- drug_data_todo %>%
+  group_by(UNIT_DATE) %>%
+  summarise(count_n = n_distinct(PERSON_ID), count_inj = n())
+
+luce_m <- drug_data_todo %>%
+  filter(DRUG_CONCEPT_ID %in% used_lucentis_id) %>%
+  group_by(UNIT_DATE) %>%
+  summarise(count_n = n_distinct(PERSON_ID), count_inj = n())
+
+eyle_m <- drug_data_todo %>%
+  filter(DRUG_CONCEPT_ID %in% used_eylea_id) %>%
+  group_by(UNIT_DATE) %>%
+  summarise(count_n = n_distinct(PERSON_ID), count_inj = n())
+
+mergeAll_m <- merge(merge(
+  all_m,
+  rani_m, by = 'UNIT_DATE', all = TRUE),
+  afli_m, by = 'UNIT_DATE', all = TRUE)
+
+colnames(mergeAll_m) <- c("MONTH", 
+                        "ALLDRUG_N", "ALLDRUG_INJ",
+                        "RANI_N", "RANI_INJ", 
+                        "AFLI_N", "AFLI_INJ")
+
+write.csv(mergeAll_m, file = "./result_eamd/all_drug_count_by_month.csv")
+
+# Index Date(진단일) 기준 진단발생
+# select first
+drug_data_s <- selectFirst(drug_data_todo, "DRUG_EXPOSURE_START_DATE")
+
+#연도별
+drug_data_s$year <- format(as.Date(drug_data_s$DRUG_EXPOSURE_START_DATE), "%Y")
+
+all_ind <- drug_data_s %>%
+  group_by(year) %>%
+  summarise(count_n = n_distinct(PERSON_ID), count_inj = n())
+
+luce_ind <- drug_data_s %>%
+  filter(DRUG_CONCEPT_ID %in% used_lucentis_id) %>%
+  group_by(year) %>%
+  summarise(count_n = n_distinct(PERSON_ID), count_inj = n())
+
+eyle_ind <- drug_data_s %>%
+  filter(DRUG_CONCEPT_ID %in% used_eylea_id) %>%
+  group_by(year) %>%
+  summarise(count_n = n_distinct(PERSON_ID), count_inj = n())
+
+mergeAll_ind <- merge(merge(
+  all_ind,
+  rani_ind, by = 'year', all = TRUE),
+  afli_ind, by = 'year', all = TRUE)
+
+colnames(mergeAll_ind) <- c("YEAR", 
+                        "ALLDRUG_N", "ALLDRUG_INJ",
+                        "RANI_N", "RANI_INJ", 
+                        "AFLI_N", "AFLI_INJ")
+
+write.csv(mergeAll_ind, file = "./result_eamd/drug_count_by_year.csv")
+
+#월별
+drug_data_s["UNIT_DATE"] = floor_date(drug_data_s$DRUG_EXPOSURE_START_DATE, unit="month")
+
+all_ind_m <- drug_data_s %>%
+  group_by(UNIT_DATE) %>%
+  summarise(count_n = n_distinct(PERSON_ID), count_inj = n())
+
+luce_ind_m <- drug_data_s %>%
+  filter(DRUG_CONCEPT_ID %in% used_lucentis_id) %>%
+  group_by(UNIT_DATE) %>%
+  summarise(count_n = n_distinct(PERSON_ID), count_inj = n())
+
+eyle_ind_m <- drug_data_s %>%
+  filter(DRUG_CONCEPT_ID %in% used_eylea_id) %>%
+  group_by(UNIT_DATE) %>%
+  summarise(count_n = n_distinct(PERSON_ID), count_inj = n())
+
+mergeAll_ind_m <- merge(merge(
+  all_ind_m,
+  rani_ind_m, by = 'UNIT_DATE', all = TRUE),
+  afli_ind_m, by = 'UNIT_DATE', all = TRUE)
+
+colnames(mergeAll_ind_m) <- c("MONTH", 
+                          "ALLDRUG_N", "ALLDRUG_INJ",
+                          "RANI_N", "RANI_INJ", 
+                          "AFLI_N", "AFLI_INJ")
+
+write.csv(mergeAll_ind_m, file = "./result_eamd/drug_count_by_month.csv")
